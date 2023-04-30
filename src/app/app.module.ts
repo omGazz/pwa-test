43c5b685082189b8366c12c6529610759b55c2f6
mgazzola <mgazzola@datacolor.com>
2023-04-30 19:40:12 +0200

    mobile prompt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
-import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, APP_INITIALIZER, isDevMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PromptComponent } from './prompt-component/prompt-component.component';
+import { PwaService } from './services/pwa.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
+const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt();
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromptComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatBottomSheetModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
@@ -17,7 +33,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       registrationStrategy: 'registerWhenStable:30000'
     })
   ],
-  providers: [],
+  entryComponents: [
+    PromptComponent,
+  ],
+  providers: [
+    {provide: APP_INITIALIZER, useFactory: initializer, deps: [PwaService], multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
